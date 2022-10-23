55876438283494bf6ffdf5542d462cca6b8e4644
yossibank <yossibank@yahoo.co.jp>
2022-10-23 21:55:40 +0900

    Update swift.yml

--- Scripts/XcodeGen/swiftformat.sh
+++ Scripts/XcodeGen/swiftformat.sh
@@ -1,3 +1,5 @@
 if which mint >/dev/null; then
-    xcrun --sdk macosx mint run swiftformat .
+    git diff --name-only | grep .swift | while read filename; do
+        xcrun --sdk macosx mint run swiftformat "$filename"
+    done
 fi
