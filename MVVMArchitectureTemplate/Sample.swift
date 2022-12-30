57a3bf72ef3e6743e382134ba5cb67ba9fa16d26
yossibank <yossibank@yahoo.co.jp>
2022-12-30 10:11:32 +0900

    テスト

---
+++ MVVMArchitectureTemplate/Sample.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+class Sample1 {
+    func hogehoge() -> String {
+        "HOGEHOGE"
+    }
+
+    func noTest() -> String {
+        "NOTEST"
+    }
+}
+
+class Sample2 {
+    func foobar() -> String {
+        "FOOBAR"
+    }
+}
+
+class Sample3 {
+    func foo() -> String {
+        "FOO"
+    }
+}
