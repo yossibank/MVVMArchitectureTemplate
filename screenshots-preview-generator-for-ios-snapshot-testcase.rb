1d538588f70cf09c23285947473217c591cf2f61
yossibank <yossibank@yahoo.co.jp>
2022-12-31 16:49:19 +0900

    snapshotマークダウン生成コマンド修正

--- screenshots-preview-generator-for-ios-snapshot-testcase.rb
+++ screenshots-preview-generator-for-ios-snapshot-testcase.rb
@@ -4,22 +4,30 @@ PATTERN_OF_OS_VERSION = %r{\d+_\d+}
 PATTERN_OF_SCREEN_SIZE = %r{\d+x\d+}
 NUMBER_OF_ROWS = 5
 
-# Change here for number of your test case
+# マークダウンで表示する数
 NUMBER_OF_COLUMNS = 2
 
-# Change here in your favor
-IMAGE_WIDTH = 100
+# 画像の幅
+IMAGE_WIDTH = 200
 
 def createMarkdown
-    # Change here for your test scheme name
+    # スキーム名
     scheme = 'MVVMArchitectureTemplateTests'
 
+    # ReferenceImages_64のパス
     reference_image_dir = "./#{scheme}/TestSnapshot/ReferenceImages_64/*"
 
     Dir.glob(reference_image_dir).sort.each do |test_file_path|
+        # マークダウンで表示するための準備
         markdowns = ""
+
+        # ReferenceImages_64内のフォルダ名取得(「/」で区切った最後)
         test_title = test_file_path.split("/").last
+
+        # マークダウンにタイトル名追加
         markdowns += "# #{test_title}\n\n"
+
+        # ReferenceImages_64内の1フォルダ内のファイルパス取得
         reference_image_path = "#{test_file_path}/*"
 
         Dir.glob(reference_image_path).sort.each_slice(NUMBER_OF_COLUMNS) do |slice|
@@ -27,11 +35,11 @@ def createMarkdown
 
             slice.each do |screenShot|
                 tokens = screenShot[/test(.+)/, 1].split("_")
-                header = tokens[1]
+                header = tokens[1..tokens.count - 5].join("")
                 postfix = tokens[2..].join("_")
                 os = postfix.match(PATTERN_OF_OS_VERSION)[0].gsub('_', '.')
                 screenSize = convertScreenSizeIntoDeviceName(postfix.match(PATTERN_OF_SCREEN_SIZE)[0])
-                src = "#{scheme}/#{screenShot[/.\/#{scheme}\/(.+)/, 1]}"
+                src = "../#{screenShot[/.\/#{scheme}\/(.+)/, 1]}"
                 imageTag = "<img src='#{src}' width='#{IMAGE_WIDTH}' style='border: 1px solid #999' />"
 
                 rows[0].push header
@@ -64,6 +72,8 @@ def convertScreenSizeIntoDeviceName(screenSize)
         return 'iPhone XS'
     when "414x896"
         return 'iPhone XS Max'
+    when "390x844"
+        return 'iPhone14'
     else
         return 'iPhone'
     end
