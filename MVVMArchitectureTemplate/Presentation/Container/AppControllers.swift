8000e1461c2536a8934ee121e92e95b26ccf6075
yossibank <yossibank@yahoo.co.jp>
2023-05-30 22:31:09 +0900

    プレゼンテーション層整理、テストコード修正

---
+++ MVVMArchitectureTemplate/Presentation/Container/AppControllers.swift
@@ -0,0 +1,70 @@
+import SwiftUI
+
+enum AppControllers {
+    enum Sample {
+        static func Add() -> SampleAddViewController {
+            let vc = SampleAddViewController()
+
+            vc.title = "サンプル作成"
+            vc.inject(
+                contentView: .init(),
+                viewModel: .init(
+                    model: Models.Sample(),
+                    analytics: FirebaseAnalytics(screenId: .sampleAdd)
+                )
+            )
+
+            return vc
+        }
+
+        static func Detail(_ modelObject: SampleModelObject) -> SampleDetailViewController {
+            let vc = SampleDetailViewController()
+            let routing = SampleDetailRouting(viewController: vc)
+
+            vc.title = "サンプル詳細"
+            vc.inject(
+                contentView: .init(modelObject: modelObject),
+                viewModel: .init(
+                    modelObject: modelObject,
+                    routing: routing,
+                    analytics: FirebaseAnalytics(screenId: .sampleDetail)
+                )
+            )
+
+            return vc
+        }
+
+        static func Edit(_ modelObject: SampleModelObject) -> SampleEditViewController {
+            let vc = SampleEditViewController()
+
+            vc.title = "サンプル編集"
+            vc.inject(
+                contentView: .init(modelObject: modelObject),
+                viewModel: .init(
+                    model: Models.Sample(),
+                    modelObject: modelObject,
+                    analytics: FirebaseAnalytics(screenId: .sampleEdit)
+                )
+            )
+
+            return vc
+        }
+
+        static func List() -> SampleListViewController {
+            let vc = SampleListViewController()
+            let routing = SampleListRouting(viewController: vc)
+
+            vc.title = "サンプル一覧"
+            vc.inject(
+                contentView: .init(),
+                viewModel: .init(
+                    model: Models.Sample(),
+                    routing: routing,
+                    analytics: FirebaseAnalytics(screenId: .sampleList)
+                )
+            )
+
+            return vc
+        }
+    }
+}
--- MVVMArchitectureTemplateTests/Architecture/Presentation/ViewModel/Sample/SampleListViewModelTest.swift
+++ MVVMArchitectureTemplateTests/Architecture/Presentation/ViewModel/Sample/SampleListViewModelTest.swift
@@ -8,24 +8,43 @@ final class SampleListViewModelTest: XCTestCase {
     private var analytics: FirebaseAnalyzableMock!
     private var viewModel: SampleListViewModel!
 
-    func test_画面表示_成功_一覧情報を取得できること() throws {
-        // arrange
-        setupViewModel()
+    override func setUp() {
+        super.setUp()
 
+        model = .init()
+        routing = .init()
+        analytics = .init(screenId: .sampleList)
+        viewModel = .init(
+            model: model,
+            routing: routing,
+            analytics: analytics
+        )
+
+        model.getHandler = { _ in
+            Future<[SampleModelObject], AppError> { promise in
+                promise(.success([SampleModelObjectBuilder().build()]))
+            }
+            .eraseToAnyPublisher()
+        }
+
+        viewModel.input.viewDidLoad.send(())
+    }
+
+    func test_画面表示_成功_一覧情報を取得できること() throws {
         // act
         let publisher = viewModel.output.$modelObject.collect(1).first()
-        let output = try awaitOutputPublisher(publisher)
+        let output = try awaitOutputPublisher(publisher).first
 
         // assert
         XCTAssertEqual(
-            output.first,
+            output,
             [SampleModelObjectBuilder().build()]
         )
     }
 
     func test_画面表示_失敗_エラー情報を取得できること() throws {
         // arrange
-        setupViewModel(isSuccess: false)
+        viewDidLoad(isSuccess: false)
 
         // act
         let publisher = viewModel.output.$error.collect(1).first()
@@ -40,8 +59,6 @@ final class SampleListViewModelTest: XCTestCase {
 
     func test_viewWillAppear_firebaseAnalytics_screenViewイベントを送信できていること() {
         // arrange
-        setupViewModel()
-
         let expectation = XCTestExpectation(description: #function)
 
         analytics.sendEventFAEventHandler = { event in
@@ -57,9 +74,6 @@ final class SampleListViewModelTest: XCTestCase {
     }
 
     func test_barButtonTapped_routing_showAddScreenが呼び出されること() {
-        // arrange
-        setupViewModel()
-
         // act
         viewModel.input.barButtonTapped.send(())
 
@@ -69,8 +83,6 @@ final class SampleListViewModelTest: XCTestCase {
 
     func test_contentTapped_firebaseAnalytics_tapSampleListイベントを送信できていること() {
         // arrange
-        setupViewModel()
-
         let expectation = XCTestExpectation(description: #function)
 
         analytics.sendEventFAEventHandler = { event in
@@ -86,9 +98,6 @@ final class SampleListViewModelTest: XCTestCase {
     }
 
     func test_contentTapped_routing_showDetailScreenが呼び出されること() {
-        // arrange
-        setupViewModel()
-
         // act
         viewModel.input.contentTapped.send((.init(row: 0, section: 0)))
 
@@ -98,11 +107,7 @@ final class SampleListViewModelTest: XCTestCase {
 }
 
 private extension SampleListViewModelTest {
-    func setupViewModel(isSuccess: Bool = true) {
-        model = .init()
-        routing = .init()
-        analytics = .init(screenId: .sampleList)
-
+    func viewDidLoad(isSuccess: Bool = true) {
         if isSuccess {
             model.getHandler = { _ in
                 Future<[SampleModelObject], AppError> { promise in
@@ -119,10 +124,6 @@ private extension SampleListViewModelTest {
             }
         }
 
-        viewModel = .init(
-            model: model,
-            routing: routing,
-            analytics: analytics
-        )
+        viewModel.input.viewDidLoad.send(())
     }
 }
