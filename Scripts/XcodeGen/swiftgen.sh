c0a5b0c8ef8948ad3e5346d74825626733a6965d
yossibank <yossibank@yahoo.co.jp>
2023-06-08 10:48:53 +0900

    homebrewのPath修正

--- Scripts/XcodeGen/swiftgen.sh
+++ Scripts/XcodeGen/swiftgen.sh
@@ -1,3 +1,8 @@
+# Apple Silicon MacでHomebrewのPATHを通す
+if [ -d /opt/homebrew/bin ] && ! type brew > /dev/null 2>&1 ; then
+    export PATH="$PATH:/opt/homebrew/bin"
+fi
+
 if which mint >/dev/null; then
     xcrun --sdk macosx mint run swiftgen config run --config swiftgen.yml
 fi
