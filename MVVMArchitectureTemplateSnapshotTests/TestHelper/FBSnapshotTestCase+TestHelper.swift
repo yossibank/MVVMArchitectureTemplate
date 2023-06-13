126e62adc562675eca97afb14ef3dcf0169322bb
yossibank <yossibank@yahoo.co.jp>
2023-06-14 00:09:28 +0900

    スナップショット撮影時callViewControllerAppear呼び出し

--- MVVMArchitectureTemplateSnapshotTests/TestHelper/FBSnapshotTestCase+TestHelper.swift
+++ MVVMArchitectureTemplateSnapshotTests/TestHelper/FBSnapshotTestCase+TestHelper.swift
@@ -84,6 +84,8 @@ private extension FBSnapshotTestCase {
 
         viewAction?()
 
+        callViewControllerAppear(vc: window.rootViewController!)
+
         wait(timeout: viewAfter + 3.0) { expectation in
             Task { @MainActor in
                 try await Task.sleep(seconds: viewAfter)
